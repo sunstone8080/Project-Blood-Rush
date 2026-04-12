c6ec55a01622fcff7e18cd3d4c4473c4f279d261
Cami Alarcon-Fernandez <camialarconfern@gmail.com>
2026-04-12 16:24:15 -0400

    Implemented basic dialogue functionality

--- Project Blood Rush/Assets/NPC.cs
+++ Project Blood Rush/Assets/NPC.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    public TextAsset dialogueFile;
 
+    public DialogueUiManager _dialogueUiManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        if (_dialogueUiManager == null)
+        {
+            Debug.LogError("DialogueUiManager reference is null.");
+            return;
+        }
+        _dialogueUiManager.StartDialogue(dialogueFile);
     }
 }
