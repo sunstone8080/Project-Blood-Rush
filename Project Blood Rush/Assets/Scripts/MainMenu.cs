e061890c2142e03744e41cfd6a40f45bc64bee16
Cami Alarcon-Fernandez <camialarconfern@gmail.com>
2026-04-24 19:30:54 -0400

    Added basic main menu, start game button leads to game

---
+++ Project Blood Rush/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
